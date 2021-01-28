80e5c635ce6ba9143ecfde1581b938dcb50fee08
hanaaple <hanaaple@ajou.ac.kr>
2021-01-28 16:32:23 +0900

    플러그인 추가

--- Assets/Scripts/Common.cs
+++ Assets/Scripts/Common.cs
@@ -2,17 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Common : MonoBehaviour
+public abstract class Common : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
